cc7407de2ad787b9ec4c5bee8c064c140e02c9a7
Angel Guerra <santi.gammer88@hotmail.com>
2023-09-14 22:34:02 -0500

    Primer commit

---
+++ 2Condicionales/app.js
@@ -0,0 +1,77 @@
+/*let day = 'miercoles';
+
+switch (day) {
+    case 'lunes':
+        console.log('Sumergirme en mi propia miseria');
+        break;
+    case 'martes':
+        console.log('contemplar el cielo');
+        break;
+    case 'miercoles':
+        console.log('llorar escuchando jose jose');
+        break;
+    case 'jueves':
+        console.log('Ejercicio');
+        break;
+        default:
+            console.log('ese no es un dia de la semana');
+}
+*/
+/*
+let mes = 2
+
+
+switch (mes) {
+    case '1':
+        console.log('enero');
+        break;
+    case '2':
+        console.log('febrero');
+        break;
+    case '3':
+        console.log('marzo');
+        break;
+    case '4':
+        console.log('abril');
+        break;
+        default:
+            console.log('mayo');
+}*/
+
+//error//
+/*
+const operador=prompt('escoje un operador que sea -o+');
+const numero1=prompt('dame 1 numero');
+const numero2=prompt('dame otro numero');
+
+if(operador==='+')
+console.log('la suma de','numero1','numero2','es igual a','numero1 + numero2');
+else if(operador==='-'){
+    console.log('la suma de','numero1','numero2','es igual a','numero1 - numero2');
+};
+*/
+//error//
+
+/*
+const age=20
+let drink;
+if (age>=18){
+drink='Vodka'
+} else{
+    drink='awita'
+}
+//condicion                 true    false//
+let drinkOT = age >= 18 ? 'vino' : 'awita'
+
+console.log(drink);
+console.log(drinkOT);
+*/
+
+const avgPopulation=33;
+const mxPopulation=10;
+let isMxAbove = mxPopulation >= 33 ? 
+'encima':
+'debajo';
+
+console.log(`mexico tiene una poblacion por ${isMxAbove} del promedio`)
+
---
+++ app.js
@@ -0,0 +1,41 @@
+
+/*let isPremium=false;
+
+if(isPremium===false){
+console.log('Anuncio!');
+console.log('reproduciendo video');
+}else{
+    console.log('fin');
+}*/
+
+/*
+let age=18;
+if(age>=18){
+    console.log('Quieres un vino?');
+} else{
+    console.log('Quieres aguita?')
+}*/
+
+/*let semaforo = prompt('de que color esta el semaforo?');
+if (semaforo!=='') {
+    if (semaforo === verde); {
+        console.log('avanza');
+    } else if (semaforo === 'amarillo') {
+        console.log('PASALE PORQ SE VA A PONER EN ROJO!!');
+    } else if (semaforo === 'rojo'); {
+        console.log('detente');
+    } else {
+        alert('escribe en minusculas con un color valido >:V');
+    }
+}; else{
+    alert('ingresa un color');
+};*/
+
+/*
+let numero = prompt('Dame un numero');
+if (numero % 2 === 0) {
+    console.log('el numero', numero, 'es par');
+}else {
+        console.log('el numero', numero, 'es impar');
+    };*/
+
